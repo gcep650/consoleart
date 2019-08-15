6ab63dd1b53242c2f49823275eaa5b71809b62ad
gcep650 <gcep650@learner.dvusd.org>
2019-08-15 09:20:44 -0700

    m

--- Program1/Program.cs
+++ Program1/Program.cs
@@ -11,7 +11,7 @@ namespace Program1
     {
         static void Main(string[] args)
         {
-            CreateQuestion("Last Name");
+            CreateQuestion("Last Names");
             string last = Console.ReadLine();
 
             CreateQuestion("First Name");
